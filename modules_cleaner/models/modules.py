f81fcde14bf4a523b6f92b2e8ff5da74f37a6b32
Benoît Vézina <benoit@vezina.biz>
2022-10-24 11:40:54 -0400

    started

--- modules_cleaner/models/modules.py
+++ modules_cleaner/models/modules.py
@@ -1,7 +1,20 @@
 # -*- coding: utf-8 -*-
 
-# from odoo import models, fields, api
+from odoo import models, fields, api
+from odoo.modules.module import get_module_path
 
+
+class IrModule(models.Model):
+    _inherit = "ir.module.module"
+
+    def _check_path_exist(self):
+        for module in self:
+            print(get_module_path(module.name, display_warning=False))
+            module.path_exist = get_module_path(module.name, display_warning=False) or False
+            # if get_module_path(module.name, display_warning=False) or False:
+            #     module.unlink()
+
+    path_exist = fields.Boolean('Path exist', compute='_check_path_exist')
 
 # class ../dur_pro/dur-pro/bemade-tools/modules_cleaner(models.Model):
 #     _name = '../dur_pro/dur-pro/bemade-tools/modules_cleaner.../dur_pro/dur-pro/bemade-tools/modules_cleaner'
