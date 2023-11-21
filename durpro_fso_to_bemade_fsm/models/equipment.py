8affa2c1ac199f0cf5e2582aa009418b33d93d54
Marc Durepos <marc@bemade.org>
2023-11-21 12:22:47 -0500

    Transfer attachments and chatter from fso to fsm equipments.

--- durpro_fso_to_bemade_fsm/models/equipment.py
+++ durpro_fso_to_bemade_fsm/models/equipment.py
@@ -21,15 +21,23 @@ class Equipment(models.Model):
 
     @converter
     def copy_as_fsm(self):
-        res = self.env['bemade_fsm.equipment'].create([{
-            'pid_tag': r.pid_tag,
-            'name': r.name,
-            'complete_name': r.complete_name,
-            'tag_ids': [Command.set(r.tag_ids.copy_as_fsm().ids)],
-            'partner_location_id': r.partner_location_id.id,
-            'location_notes': r.location_notes,
-            # task_ids left blank as set in the intervention
-        } for r in self])
+        res = self.env['bemade_fsm.equipment']
+        for r in self:
+            rec = self.env['bemade_fsm.equipment'].create({
+                'pid_tag': r.pid_tag,
+                'name': r.name,
+                'complete_name': r.complete_name,
+                'tag_ids': [Command.set(r.tag_ids.copy_as_fsm().ids)],
+                'partner_location_id': r.partner_location_id.id,
+                'location_notes': r.location_notes,
+                # task_ids left blank as set in the intervention
+            })
+            attachments = self.env['ir.attachment'].search([('res_model', '=', 'durpro_fso.equipment'),
+                                                            ('res_id', '=', r.id)])
+            attachments.sudo().write({'res_model': 'bemade_fsm.equipment', 'res_id': rec.id})
+            r.message_change_thread(rec)
+            res |= rec
+
         # Locations with equipment should be of company type, convert them here
         self.mapped('converted').mapped('partner_location_id').\
             write({'company_type': 'company'})
