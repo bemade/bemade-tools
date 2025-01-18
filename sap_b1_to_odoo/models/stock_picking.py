0f80bd0c75c344b9ab9da3bc4ca1cb471a1a9c6c
Marc Durepos <marc@bemade.org>
2025-01-18 12:54:38 -0500

    fix calculated field on pickings for sap

--- sap_b1_to_odoo/models/stock_picking.py
+++ sap_b1_to_odoo/models/stock_picking.py
@@ -9,10 +9,10 @@ class StockPicking(models.Model):
     def _compute_sap_orders(self):
         for picking in self:
             orders = []
-            sale = picking.sale_id and picking.sale_id.sap_docnum
-            purchase = picking.purchase_id and picking.purchase_id.sap_docnum
+            sale = str(picking.sale_id and picking.sale_id.sap_docnum)
+            purchase = str(picking.purchase_id and picking.purchase_id.sap_docnum)
             if sale:
-                orders.append(sale.name)
+                orders.append(sale)
             if purchase:
-                orders.append(purchase.name)
+                orders.append(purchase)
             picking.sap_orders = ", ".join(orders) if orders else ""
