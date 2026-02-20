340a6a95943b54d00c5b319441cb8b10a103984f
Marc Durepos <marc@bemade.org>
2026-02-20 12:38:12 -0500

    fix(qbo_to_odoo): update RETRYABLE_ERRORS → RETRYABLE_DB_ERRORS after etl_framework rename

--- qbo_to_odoo/models/pipelines/vendor_credit_etl.py
+++ qbo_to_odoo/models/pipelines/vendor_credit_etl.py
@@ -10,7 +10,7 @@ from typing import Dict, List, Optional
 
 from odoo import models
 
-from odoo.addons.etl_framework import ETL, ETLContext, RETRYABLE_ERRORS
+from odoo.addons.etl_framework import ETL, ETLContext, RETRYABLE_DB_ERRORS
 
 from .utils import get_api_client
 
@@ -294,7 +294,7 @@ class QboVendorCreditImporter(models.AbstractModel):
             try:
                 move.action_post()
                 posted += 1
-            except RETRYABLE_ERRORS:
+            except RETRYABLE_DB_ERRORS:
                 raise
             except Exception as e:
                 _logger.warning(f"Could not post vendor credit {vals.get('ref')}: {e}")
