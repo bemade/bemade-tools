27342ac0cfff39cbb9e05d34e0011e87fb124efd
Marc Durepos <marc@bemade.org>
2026-05-22 13:00:29 -0400

    fix(sap_b1_to_odoo): register payment-term test module in __init__.py imports

--- sap_b1_to_odoo/tests/__init__.py
+++ sap_b1_to_odoo/tests/__init__.py
@@ -6,5 +6,6 @@ from .pipelines import (
     test_account_move_jdt1_etl,
     test_jdt1_sale_link,
     test_product_pricelist_etl,
+    test_res_partner_payment_term,
     test_res_partner_pricelist,
 )
