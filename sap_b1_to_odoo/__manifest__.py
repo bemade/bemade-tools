83400a05039d0d6817d868e6dad020a9a75aa160
Marc Durepos <marc@bemade.org>
2026-06-30 20:59:59 -0400

    chore(sap_b1_to_odoo): bump manifest version to 19.0.0.0.9

--- sap_b1_to_odoo/__manifest__.py
+++ sap_b1_to_odoo/__manifest__.py
@@ -19,7 +19,7 @@
 #
 {
     "name": "SAP Business One to Odoo",
-    "version": "19.0.0.0.8",
+    "version": "19.0.0.0.9",
     "summary": "Convert a database from SAP Business One to Odoo",
     "category": "Technical",
     "author": "Bemade Inc.",
